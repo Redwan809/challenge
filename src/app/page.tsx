094f41e8693bf2a37b18ee144d9013309a4f37ad
Redwan Islam <redwanislam791@gmail.com>
2025-10-17 06:09:02 +0000

    Rename  Games to bet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             <SidebarMenuItem>
               <SidebarMenuButton>
                 <Puzzle />
-                Games
+                bet
               </SidebarMenuButton>
             </SidebarMenuItem>
           </SidebarMenu>
