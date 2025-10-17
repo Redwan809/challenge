4814d0df686d73521f930226fe7204a9ded1d59d
Redwan Islam <redwanislam791@gmail.com>
2025-10-17 06:30:23 +0000

    স্ক্রিনের জুম করা যাবে না। এবং স্করল করতে পারবে না।

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,14 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased overflow-hidden">
         <Header />
-        <div className="pb-20">
+        <div className="h-screen overflow-y-auto pb-20">
           {children}
         </div>
         <BottomNav />
